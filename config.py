f99d88dc7e2993cf30e8ed3f64d29121ee8f7af6
Giampiero <giampiero7@users.noreply.github.com>
2018-12-17 16:52:07 +0100

    Added documentation in config.py

--- config.py
+++ config.py
@@ -1,16 +1,31 @@
+# Access point WiFi name (SSID)
 AP_SSID = 'Exo_AP'
-AP_PASSWORD = 'exosense'
-AP_CHANNEL = 7
-AP_ON_TIME_SEC = 600 # No less than 120 (2 mins)
-AP_ON_MB_TIMEOUT_SEC = 300 # Set to 0 to disable
 
+# Access point WiFi password
+AP_PASSWORD = 'exosense'
+
+# Access point WiFi channel
+AP_CHANNEL = 7
+
+# DUration of access point mode, is seconds.
+# Values below 120 (2 mins) are ignored.
+AP_ON_TIME_SEC = 600
+
+# Time (seconds) after power-on after which access point mode is enabled
+# if the module is configured and no Modbus request is received.
+# Set to 0 to disable.
+AP_ON_MB_TIMEOUT_SEC = 300
+
+# FTP server credentials
 FTP_USER = 'exo'
 FTP_PASSWORD = 'sense'
 
-MB_ADDRESS = 10
+# Modbus parameters
+MB_ADDRESS = 10 # Set to 0 to boot as not configured (AP on)
 MB_BAUDRATE = 19200
 MB_DATA_BITS = 8
 MB_STOP_BITS = 1
 MB_PARITY = 1 # 1 = even, 2 = odd, 3 = none
 
+# Temperature offset (°C)
 TEMP_OFFSET = 0
