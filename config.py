6dd5b2ec64683b2cac4a4e9f469fde93a7861de2
Giampiero <giampiero7@users.noreply.github.com>
2019-01-31 19:06:09 +0100

    Updated default configuration

--- config.py
+++ config.py
@@ -7,25 +7,42 @@ AP_PASSWORD = 'exosense'
 # Access point WiFi channel
 AP_CHANNEL = 7
 
-# DUration of access point mode, is seconds.
+# Duration of access point mode, is seconds.
 # Values below 120 (2 mins) are ignored.
 AP_ON_TIME_SEC = 600
 
 # Time (seconds) after power-on after which access point mode is enabled
-# if the module is configured and no Modbus request is received.
-# Set to 0 to disable.
-AP_ON_MB_TIMEOUT_SEC = 300
+# if the module is configured and no Modbus RTU request is received or
+# if cannot connect to WiFi (Modbus TCP).
+# Set to 0 to disable access point mode.
+AP_ON_TIMEOUT_SEC = 300
 
 # FTP server credentials
 FTP_USER = 'exo'
 FTP_PASSWORD = 'sense'
 
-# Modbus parameters
-MB_ADDRESS = 10 # Set to 0 to boot as not configured (AP on)
-MB_BAUDRATE = 19200
-MB_DATA_BITS = 8
-MB_STOP_BITS = 1
-MB_PARITY = 1 # 1 = even, 2 = odd, 3 = none
-
 # Temperature offset (°C)
 TEMP_OFFSET = 0
+
+# Modbus RTU parameters
+MB_RTU_ADDRESS = 0 # Set to 0 to disable
+MB_RTU_BAUDRATE = 19200
+MB_RTU_DATA_BITS = 8
+MB_RTU_STOP_BITS = 1
+MB_RTU_PARITY = 1 # 1 = even, 2 = odd, 3 = none
+
+# Modbus TCP parameters
+
+MB_TCP_IP = '' # Modbus TCP disabled
+#MB_TCP_IP = 'dhcp' # Use DHCP to obtain IP address
+#MB_TCP_IP = '192.168.1.100' # Static IP address
+
+MB_TCP_MASK = '255.255.255.0' # Network mask
+MB_TCP_GW = '192.168.1.1' # Gateway IP address
+MB_TCP_DNS = '192.168.1.1' # DNS IP address
+
+MB_TCP_PORT = 502 # Port for Modbus TCP requests
+
+MB_TCP_WIFI_SSID = 'MyWiFi' # SSID of the WiFi network to connect to
+MB_TCP_WIFI_PWD = 's3cretP4ssw0rd' # WiFi password
+MB_TCP_WIFI_SEC = 3 # WiFi security: 0 = Open, 1 = WEP, 2 = WPA, 3 = WPA2
